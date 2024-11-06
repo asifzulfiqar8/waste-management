fd2878a6da0cf4b5304e2f389c94bafba39dd9b1
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-11-06 11:28:43 +0500

    svgs are added

---
+++ src/assets/svgs/icon.js
@@ -0,0 +1,7 @@
+import DashboardIcon from "./aside/DashboardIcon";
+import SensorsIcon from "./aside/SensorsIcon";
+import DriverIcon from "./aside/DriverIcon";
+import TruckIcon from "./aside/TruckIcon";
+import BinIcon from "./aside/BinIcon";
+
+export { DashboardIcon, SensorsIcon, DriverIcon, TruckIcon, BinIcon };
